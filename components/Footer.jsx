70958af1c6581ee500b4bea22e52795abee7e9a9
Abhishek Pandit <abhishekbuttu@gmail.com>
2022-07-03 16:11:38 +0530

    project name change

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,7 +2,7 @@ import { Box } from '@chakra-ui/layout';
 
 const Footer = () => (
   <Box textAlign='center' p='5' color='gray.600' borderTop='1px' borderColor='gray.100'>
-    © 2022 RealEstate, Inc.
+    © 2022 HomeSphere, Inc.
   </Box>
 );
 
